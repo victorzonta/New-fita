13e47c45bb85b87bca86c74c293cc4f6b086dbe2
victorzonta <zontavlogs77@gmail.com>
2021-05-17 14:53:41 -0300

    fita

---
+++ src/zonta.h
@@ -0,0 +1,119 @@
+char roxo[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: purple;">
+</body>
+</html>
+)=====";
+
+char amarelo[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: yellow;">
+</body>
+</html>
+)=====";
+
+char verde[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: green;">
+</body>
+</html>
+)=====";
+
+char off[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: black;">
+</body>
+</html>
+)=====";
+
+char on[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: white;">
+</body>
+</html>
+)=====";
+
+char azul[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: blue;">
+</body>
+</html>
+)=====";
+
+char branco[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: white;">
+</body>
+</html>
+)=====";
+
+char vermelho[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: red;">
+</body>
+</html>
+)=====";
+
+char rochao[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color:#4B0082;">
+</body>
+</html>
+)=====";
+
+char laranja [] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: orange;">
+</body>
+</html>
+)=====";
+
+char rosa[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: #FF00FF;">
+</body>
+</html>
+)=====";
+
+char verdin[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body style = "background-color: #00FFFF;">
+</body>
+</html>
+)=====";
